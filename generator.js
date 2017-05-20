759e1a8373a643f093dc039ff1562c50ec383b6d
Leonardo Grasso <me@leonardograsso.com>
2017-05-20 22:23:17 +0200

    New: Generator function

---
+++ generator.js
@@ -0,0 +1,11 @@
+export const generator = function *(set, map = (value) => value) {
+
+  yield map([], 0) // the empty set
+
+  const cn = Math.pow(2, set.length)
+  for (let c = 1; c < cn; c++) {
+    yield map(set.filter((value, index) => Math.pow(2, index) & c), c)
+  }
+}
+
+export default generator
